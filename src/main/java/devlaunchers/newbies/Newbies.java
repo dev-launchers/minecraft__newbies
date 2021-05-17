6691f13693bd43663081c7f05e737b011eeb323e
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-05-17 15:40:54 +0530

    Implement newbie guide with custom actions parser!
    Two commands OPEN_FILE and SUGGEST_COMMAND isnt quite giving anything, I am pretty sure it isnt a bug but maybe wrong use of Command values, So these two commands might do nothing right now and might still need to be searched for what they actually do!

--- src/main/java/devlaunchers/newbies/Newbies.java
+++ src/main/java/devlaunchers/newbies/Newbies.java
@@ -2,12 +2,20 @@ package devlaunchers.newbies;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Newbies extends JavaPlugin implements Listener {
@@ -44,9 +52,44 @@ public final class Newbies extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         final Player player = event.getPlayer();
-        if(!player.hasPlayedBefore()) {
+        if(!player.hasPlayedBefore() || true) { // TODO: Change
             getLogger().info("New player detected: " + player.getName());
-            this.starterPack.forEach((item) -> player.getInventory().addItem(new ItemStack(item)));
+            //this.starterPack.forEach((item) -> player.getInventory().addItem(new ItemStack(item)));
+            for(ItemStack i : starterPack){
+                ItemStack it = new ItemStack(i);
+                if(it.getType() == Material.WRITTEN_BOOK){
+                    i.setItemMeta(extractActionsAndParse(it));
+                }
+                player.getInventory().addItem(it);
+            }
         }
+    }
+
+    // Function which parse for events in book pages, Conventional minecraft serializer doesnt support events
+    // so this function can be used to have actions for you! To add an event just edit the page text and in the
+    // start of line write in this format (<Command Name>__<Command Value>)<Your Text Here>...
+    // Command Name is simply name of Enums in ClickEvent Action class like: ClickEvent.Action.<Command Name>,
+    // List of valid commands: OPEN_URL, OPEN_FILE, RUN_COMMAND, SUGGEST_COMMAND, CHANGE_PAGE, COPY_TO_CLIPBOARD
+    // Mind the opening and closing brackets and always write this markup in start of line and see config.yml
+    // for sample!!
+    Pattern pattern = Pattern.compile("^\\((.*?)__(.*?)\\)");
+    private BookMeta extractActionsAndParse(ItemStack it) {
+        BookMeta bm = (BookMeta) it.getItemMeta();
+        int pageNo = 1;
+        for(String page : bm.getPages()){
+            ComponentBuilder p;
+            Matcher matcher = pattern.matcher(page);
+            if (matcher.find())
+            {
+                String c = page.replace(matcher.group(), "");
+                p = new ComponentBuilder(c).event(new ClickEvent(ClickEvent.Action.valueOf(matcher.group(1)), matcher.group(2)));;
+            }
+            else{
+                p = new ComponentBuilder(page);
+            }
+            bm.spigot().setPage(pageNo, p.create());
+            pageNo++;
+        }
+        return bm;
     }
 }
